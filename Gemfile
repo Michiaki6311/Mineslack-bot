2fc96c059d69476e42347f5342d452c8c3c9f6d5
Michiaki <kanadebito63@yahoo.co.jp>
2016-08-26 13:17:39 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'sinatra'
+gem 'slack-api'
+gem 'json'
+gem 'nokogiri'
+gem 'sinatra-contrib'
+gem 'slack-incoming-webhooks'
+gem 'rack'
---
+++ app.rb
@@ -0,0 +1,52 @@
+require 'sinatra'
+require 'json'
+require 'open-uri'
+require 'uri'
+require 'nokogiri'
+require 'slack/incoming/webhooks'
+
+get '/' do
+  'Hello,World!'
+end
+
+post '/search' do
+    if params[:text] =~ /^mr\s/ then
+      
+      response = 'Hi,Michi!'
+      message = "exist"
+      array = []
+      url = "http://www26.atwiki.jp/minecraft/pages/1073.html"
+      items = nil
+      searchword = params[:text].gsub(/^mr\s/,'')
+
+      # parse only first time and keep items
+      if !items
+        doc = Nokogiri::HTML.parse(open(url), nil, "utf-8")
+        items = doc.xpath("//tr").select { |tr|
+          # FIXME: rowspanned item is not contained
+          tr.children.filter('td').length == 3
+        }.map { |tr|
+          item = tr.children.filter('td')
+
+          {
+            name: item[0].text,
+            image: item[1].xpath('.//img').map {|img| "http:#{img.attribute('src')}#.png" }.join("\n"),
+            craft: item[2].text,
+          }
+        }
+      end
+
+      items.each do |x|
+        if x[:name] =~ /#{searchword}/
+          array.push("#{x[:name]}\n#{x[:craft]}\n#{x[:image]}\n")
+        end
+      end
+        
+        response = array.join.strip
+        response = array.sample if response.strip.length > 1024
+        response = "Not Found" if array == [] && message == "exist"
+        
+        slack = Slack::Incoming::Webhooks.new ENV['URL']
+        slack.post "#{response.strip}"
+    end
+end
