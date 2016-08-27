cfe8972f4b95440593c8f1a67b7e6918d68074b3
Michiaki <kanadebito63@yahoo.co.jp>
2016-08-27 02:47:02 +0000

    add photo search command

--- app.rb
+++ app.rb
@@ -57,8 +57,55 @@ post '/search' do
         slack.post "#{response.strip}"
       end
         
-        elsif params[:text] =~ /^!mg\s/ then
-        response = "hi"
+        
+        
+        
+        
+    elsif params[:text] =~ /^!mg\s/ then
+      array = []
+      url = "http://www26.atwiki.jp/minecraft/pages/1073.html"
+      items = nil
+      timestamp = Time.now.to_i
+      searchword = params[:text].gsub(/^!mg\s/,'')
+
+      # parse only first time and keep items
+      if !items
+        doc = Nokogiri::HTML.parse(open(url), nil, "utf-8")
+        items = doc.xpath("//tr").select { |tr|
+          # FIXME: rowspanned item is not contained
+          tr.children.filter('td').length == 3
+        }.map { |tr|
+          item = tr.children.filter('td')
+
+          {
+            name: item[0].text,
+            image: item[1].xpath('.//img').map {|img| "http:#{img.attribute('src')}##{timestamp}" }.join("\n"),
+          }
+        }
+      end
+
+      items.each do |x|
+        if x[:name].downcase! =~ /#{searchword}/
+          array.push("#{x[:name]}\n#{x[:image]}\n")
+        end
+      end
+        
+        response = array.join.strip
+        response = "Not Found" if array == []
+        
+      if params[:token] == ENV['TOKEN1']
+        slack = Slack::Incoming::Webhooks.new ENV['URL']
+        slack.post "#{response.strip}"
+        elsif params[:token] == ENV['TOKEN2']
+        slack = Slack::Incoming::Webhooks.new ENV['URL2']
+        slack.post "#{response.strip}"
+        elsif params[:token] == ENV['TOKEN3']
+        slack = Slack::Incoming::Webhooks.new ENV['URL3']
+        slack.post "#{response.strip}"
+      end
+      
+    else ""
+      
     end
     
 end
