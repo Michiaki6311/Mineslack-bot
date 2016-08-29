cd2811a9654b6296c99ae0750dc62d169892a557
Michiaki <kanadebito63@yahoo.co.jp>
2016-08-29 07:14:52 +0000

    add posts new thread

--- app.rb
+++ app.rb
@@ -95,18 +95,24 @@ post '/search' do
         end
       end
         
-        response = array.join.strip
+        response = array
         response = "Not Found" if array == []
         
       if params[:token] == ENV['TOKEN1']
         slack = Slack::Incoming::Webhooks.new ENV['URL']
-        slack.post "#{response.strip}"
+        array.map{|arr|
+        slack.post "#{arr.strip}"
+        }
         elsif params[:token] == ENV['TOKEN2']
         slack = Slack::Incoming::Webhooks.new ENV['URL2']
-        slack.post "#{response.strip}"
+        array.map{|arr|
+        slack.post "#{arr.strip}"
+        }
         elsif params[:token] == ENV['TOKEN3']
         slack = Slack::Incoming::Webhooks.new ENV['URL3']
-        slack.post "#{response.strip}"
+        array.map{|arr|
+        slack.post "#{arr.strip}"
+        }
       end
       
     else ""
