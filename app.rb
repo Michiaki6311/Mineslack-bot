6141b0e35d8cd2e71b063b59417b3e5a7ed3cef9
Michiaki <kanadebito63@yahoo.co.jp>
2016-09-11 09:06:45 +0000

    edit formation

--- app.rb
+++ app.rb
@@ -151,7 +151,7 @@ post '/search' do
 					{
 						name: node.xpath("//h1").text,
 						image: node.xpath("//div[@class='infobox-imagearea']//img").attribute('src').value + "##{timestamp}",
-						description: node.xpath("//div[@class='mw-content-ltr']/h3/span[@class='mw-headline']|//div[@class='mw-content-ltr']/h2/span[@class='mw-headline']|//div[@class='mw-content-ltr']/p|//div[@class='mw-content-ltr']/ul/li[not(@class) and not(*)]").map{|new_node|
+						description: node.xpath("//div[@class='mw-content-ltr']/h3|//div[@class='mw-content-ltr']/h2|//div[@class='mw-content-ltr']/p|//div[@class='mw-content-ltr']/ul/li[not(@class) and not(*)]").map{|new_node|
 						if new_node.to_html =~ /<h2/ then
 							case new_node.text
 							when /歴史|ギャラリー|脚注/
