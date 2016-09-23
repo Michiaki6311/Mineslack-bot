e4d7ea49e4afda36bedce1a68bf72e20f0b8e0f2
Michiaki <kanadebito63@yahoo.co.jp>
2016-09-23 06:15:27 +0000

    subdivision

--- app.rb
+++ app.rb
@@ -58,7 +58,21 @@ post '/search' do
     
     # Slackへの投稿
     
-	slack = Slack::Incoming::Webhooks.new ENV['URL3']
-	slack.post "hi"
+	if params[:token] == ENV['TOKEN1']
+			slack = Slack::Incoming::Webhooks.new ENV['URL']
+			item_details.map{|arr|
+				slack.post "#{arr.strip}"
+			}
+		elsif params[:token] == ENV['TOKEN2']
+			slack = Slack::Incoming::Webhooks.new ENV['URL2']
+			item_details.map{|arr|
+				slack.post "#{arr.strip}"
+			}
+		elsif params[:token] == ENV['TOKEN3']
+			slack = Slack::Incoming::Webhooks.new ENV['URL3']
+			item_details.map{|arr|
+				slack.post "#{arr.strip}"
+			}
+		end
     
 end
